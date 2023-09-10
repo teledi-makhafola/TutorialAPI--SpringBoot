106f80bd87c1c5b5463f8b789adbcfca90d5d29e
teledi-makhafola <lkgahliso03@gmail.com>
2023-09-10 17:37:06 +0200

    first initial commit

---
+++ src/main/java/com/basic/crudproject/CrudprojectApplication.java
@@ -0,0 +1,13 @@
+package com.basic.crudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/crudproject/CrudprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.crudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
